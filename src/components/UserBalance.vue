c0e598af7679f8dff034bf2f7c1e8911df934b51
josedmaza <josedmaza21@gmail.com>
2020-12-15 22:07:31 -0500

    Conexion con heroku

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@
           this.username = this.$route.params.username
 
           let self = this
-          axios.get("https://cajero-api.herokuapp.com/user/balance/" + this.username)
+          axios.get("https://cajero-app1995.herokuapp.com/user/balance/" + this.username)
               .then((result) => {
                   self.balance = result.data.balance
               })
