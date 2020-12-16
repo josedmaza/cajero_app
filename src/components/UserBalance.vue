f0ec96ac9209ce131dcbf1efff31c9baceb7bd2c
josedmaza <josedmaza21@gmail.com>
2020-12-15 22:31:37 -0500

    Tercer intento de conexion

--- src/components/UserBalance.vue
+++ src/components/UserBalance.vue
@@ -20,7 +20,7 @@
           this.username = this.$route.params.username
 
           let self = this
-          axios.get("https://cajero-api1234.herokuapp.com//user/balance/" + this.username)
+          axios.get("https://cajero-api1234.herokuapp.com/user/balance/" + this.username)
               .then((result) => {
                   self.balance = result.data.balance
               })
